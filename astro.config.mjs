e4292975c190f3a2564cfa7ec66acb3784634e79
Bhavantu <santiagoeste@gmail.com>
2026-06-11 19:59:44 +1200

    optimizacion ads

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()]
+  site: 'https://blogcito.com',
+  integrations: [sitemap()],
 });
